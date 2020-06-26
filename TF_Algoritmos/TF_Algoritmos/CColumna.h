1d20db72cb488584adb57c906e4967c760b4d110
u20181a686 (Tiglla Arrascue, Bruno) <u20181a686@upc.edu.pe>
2020-06-26 03:03:47 -0500

    .

--- TF_Algoritmos/TF_Algoritmos/CColumna.h
+++ TF_Algoritmos/TF_Algoritmos/CColumna.h
@@ -1,7 +1,34 @@
 #ifndef __CCOLUMNA_H__
 #define __CCOLUMNA_H__
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <map>
+#include <vector>
+using namespace std;
+
+class CColumna {
+private:
+	string nombre;
+	bool numero;
+	int filas;
+	vector<string> data;
+	map<string, int> dato;
+	vector<int> vFila;
+
+public:
+	CColumna(bool numero = false, int filas = 0): numero(numero), filas(filas) {}
+	~CColumna(){}
 
 
+	bool getMayor();
+	bool getMenor();
+	bool first();
+	bool last();
+
+
+};
+	
 
 
 
