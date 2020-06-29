9157bac4c141a333a96bb9f4ef7b18c9c86e31d5
u20181a686 (Tiglla Arrascue, Bruno) <u20181a686@upc.edu.pe>
2020-06-29 02:42:04 -0500

    .

--- TF_Algoritmos/TF_Algoritmos/CColumna.h
+++ TF_Algoritmos/TF_Algoritmos/CColumna.h
@@ -14,17 +14,100 @@ private:
 	int filas;
 	vector<string> data;
 	map<string, int> dato;
-	vector<int> vFila;
+	vector<int> vfila;
 
 public:
 	CColumna(bool numero = false, int filas = 0): numero(numero), filas(filas) {}
 	~CColumna(){}
 
 
-	bool getMayor();
-	bool getMenor();
-	bool first();
-	bool last();
+	vector<int> Ordenar()
+	{
+		vector<string> datatmp = data;
+		quicksort(datatmp, datatmp[0], datatmp[datatmp.size() - 1]);
+		for (int i = 0; i < datatmp.size(); i++) {
+			for (int j = 0; j < data.size(); j++) {
+				if (datatmp[i] == data[j]) {
+					vfila.push_back(j);
+				}
+			}
+		}
+		return vfila;
+	}
+	void quicksort(vector<string>& names, string min, string max) {
+		vector<string> tempData;
+		int temp = 0, i = 0;
+		string lowMin = max,
+			lowMax = min,
+			highMin = max,
+			highMax = min,
+			pivot;
+		vector<string>  below, above;
+		if (min != max) {
+			pivot = (max[i] + min[i]) / 2;
+			while (temp < names.size()) {
+				if (names[temp] <= pivot) {
+					if (lowMax.compare(names[temp]) < 0) {
+						lowMax = names[temp];
+					}
+					if (lowMin.compare(names[temp]) > 0) {
+						lowMin = names[temp];
+					}
+					below.push_back(names[temp]);
+				}
+				else {
+					if (highMax.compare(names[temp]) < 0) {
+						highMax = names[temp];
+					}
+					if (highMin.compare(names[temp]) > 0) {
+						highMin = names[temp];
+					}
+					above.push_back(names[temp]);
+				}
+				temp++;
+			}
+			if ((below.size() > 1) && (names.size() != below.size())) {
+				quicksort(below, lowMin, lowMax);
+			}
+			if ((above.size() > 1) && (names.size() != above.size())) {
+				quicksort(above, highMin, highMax);
+			}
+			for (size_t i = 0; i < below.size(); i++) {
+				names[i] = below[i];
+			}
+			for (size_t i = below.size(); i < names.size(); i++) {
+				names[i] = above[i - below.size()];
+			}
+
+		}
+
+	}
+
+
+	bool getMayor(string var, int ind) {
+		if (data[ind].compare(var) > 0)
+			return true;
+		else
+			return false;
+	}
+	bool getMenor(string var, int ind) {
+		if (data[ind].compare(var) < 0)
+			return true;
+		else
+			return false;
+	}
+	bool front(int ind, char c) {
+		if (data[ind].front() == c)
+			return true;
+		else
+			return false;
+	}
+	bool back(int ind, char c) {
+		if (data[ind].back() == c)
+			return true;
+		else
+			return false;
+	}
 
 
 };
